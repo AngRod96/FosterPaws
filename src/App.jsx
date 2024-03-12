8017a9351fed0593f61def98cbe16b8a47b0054e
Angie Rodriguez <angie.rodriguez.167@gmail.com>
2024-03-12 15:01:07 -0500

    created the welcome page

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import { Login } from "./components/auth/Login.jsx";
 import { Routes, Route } from "react-router";
+import { Welcome } from "./components/welcome/Welcome.jsx";
 
 function App() {
   return (
     <Routes>
       <Route path="/login" element={<Login />} />
+      <Route index element={<Welcome /> } />
     </Routes>  
   )
 }
